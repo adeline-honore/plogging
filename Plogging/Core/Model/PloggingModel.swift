e2f5232fc23a45fbdecec442fc24c033b82113f4
adeline-honore <honore_adeline@yahoo.fr>
2022-10-17 11:28:13 +0200

    creation de PloggingModel

---
+++ Plogging/Core/Model/PloggingModel.swift
@@ -0,0 +1,29 @@
+//
+//  PloggingModel.swift
+//  Plogging
+//
+//  Created by HONORE Adeline on 17/10/2022.
+//
+
+import Foundation
+import MapKit
+
+struct PloggingModel {
+    let id: UUID
+    
+    let admin: String
+    
+    let date: Date
+    let hour: Double
+    
+    let place: MKMapItem
+    let coordinate: CLLocationCoordinate2D
+    
+    let annotation: MKAnnotation
+    let annotations: [MKAnnotation]
+    
+    let plogger: [String]
+    
+    var isTakingPart: Bool
+    
+}
