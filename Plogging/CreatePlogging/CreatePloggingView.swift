69afe67c8d50b3fe521d4ee34c5ea83c82154d40
adeline-honore <honore_adeline@yahoo.fr>
2022-11-14 14:20:06 +0100

    creation de CreatePlogging view et VC

---
+++ Plogging/CreatePlogging/CreatePloggingView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePloggingView.swift
+//  Plogging
+//
+//  Created by HONORE Adeline on 14/11/2022.
+//
+
+import UIKit
+
+class CreatePloggingView: UIView {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var placeTextField: UITextField!
+    
+    @IBOutlet weak var whenDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var distancePickerView: UIPickerView!
+    
+}
---
+++ Plogging/CreatePlogging/CreatePloggingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreatePloggingViewController.swift
+//  Plogging
+//
+//  Created by HONORE Adeline on 14/11/2022.
+//
+
+import UIKit
+
+class CreatePloggingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
