0b86d3ca9d0ef7abcdc0b85abcc4bcf391979754
adeline-honore <honore_adeline@yahoo.fr>
2023-11-26 10:50:40 +0100

    SwiftLint : suppression de Cocoapods et insertion par SPM

--- Podfile
+++
@@ -1,5 +0,0 @@
-platform :ios, '15.0'
-target 'Plogging' do
-  use_frameworks!
-  pod 'SwiftLint'
-end
--- Pods/Target Support Files/Pods-Plogging/Pods-Plogging-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Plogging : NSObject
-@end
-@implementation PodsDummy_Pods_Plogging
-@end
