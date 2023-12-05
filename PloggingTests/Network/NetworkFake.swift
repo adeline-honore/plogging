5a2525d46ac6f5979330283b27c1245c892cb487
adeline-honore <honore_adeline@yahoo.fr>
2023-12-05 12:04:42 +0100

    corrections pour c i 2

--- PloggingTests/Network/NetworkFake.swift
+++ PloggingTests/Network/NetworkFake.swift
@@ -5,50 +5,50 @@
 //  Created by adeline honore on 15/11/2023.
 //
 
-import Foundation
 @testable import Plogging
-import UIKit
+import Foundation
 
-class NetworkFake: NetworkProtocol {
+class PloggingNetworkFake: PloggingNetworkProtocol {
 
-    private let testCase: TestCaseNetwork
+    private let testCase: TestCase
+    private let jsonExtensionType = "json"
+    private let imageExtensionType = "jpg"
     private var isFailed: Bool = false
 
-    init(testCase: TestCaseNetwork, isFailed: Bool) {
+    init(testCase: TestCase, isFailed: Bool) {
         self.testCase = testCase
         self.isFailed = isFailed
     }
 
-    func callNetwork(router: RouterProtocol, completionHandler: @escaping (Result<Data,Error>) -> ()) {
-
+    func callNetworkGetPloggingList(router: PloggingRouterProtocol, completionHandler: @escaping (Result<[Plogging], Error>) -> Void) {
         guard !isFailed else {
             completionHandler(.failure(ErrorType.network))
             return
         }
 
-        switch testCase {
-        case .auth:
-            return completionHandler(.success(prepareAuthData()))
-        case .ploggingList:
-            return completionHandler(.success(preparePloggingListData()))
-        }
+        let data = prepareData()
+
+        let list = try? transformPloggings(data: data)
+
+        completionHandler(.success(list ?? [Plogging()]))
     }
 
-    private func prepareAuthData() -> Data {
-        let bundle = Bundle(for: NetworkFake.self)
-        let url = bundle.url(forResource: testCase.resource, withExtension: "json")!
-        guard let  authData = try? Data(contentsOf: url) else {
-            return Data()
+    func callNetworkCreateOrUpdatePlogging(plogging: Plogging, router: PloggingRouterProtocol, completionHandler: @escaping (Result<FirebaseResult, Error>) -> Void) {
+        guard !isFailed else {
+            completionHandler(.failure(ErrorType.network))
+            return
         }
-        return authData
+        return completionHandler(.success(FirebaseResult.success))
     }
 
-    private func preparePloggingListData() -> Data {
-        let bundle = Bundle(for: NetworkFake.self)
-        let url = bundle.url(forResource: testCase.resource, withExtension: "json")!
-        guard let  ploggingListData = try? Data(contentsOf: url) else {
-            return Data()
-        }
-        return ploggingListData
+    private func prepareData() -> Data {
+        let bundle = Bundle.main
+        let url = bundle.url(forResource: testCase.resource, withExtension: jsonExtensionType)!
+        guard let data = try? Data(contentsOf: url) else { return Data() }
+        return data
+    }
+
+    private func transformPloggings(data: Data) throws -> [Plogging] {
+        return try JSONDecoder().decode([Plogging].self, from: data)
     }
 }
